8ad249b0d30f1c7b4199f0f2f32f6fdaf5224b59
ltw <katarzyna.kolanek@gmail.com>
2018-05-19 21:34:40 +0200

    commit

--- upload-file-and-json.bash
+++ upload-file-and-json.bash
@@ -16,15 +16,6 @@ if [ -z "$FILE_JSON" ]; then
 	echo "ERROR: Json corresponding to input file was not specified"; exit 1
 fi
 
-
-echo "bucket:   ""$BUCKET"
-echo "file:   ""$FILE"
-echo "file_json:   ""$FILE_JSON"
-echo "------------"
-echo "file_filename:   ""$FILE_FILENAME"
-echo "file_json_filename:   ""$FILE_JSON_FILENAME"
-
-
 FILE_JSON_FILENAME=$(basename -- "$FILE_JSON")
 FILE_FILENAME=$(basename -- "$FILE")
 
